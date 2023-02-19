3cd3c9a5dde73ff6bb90b827e6d840aff9a45c78
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-19 10:19:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import{BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
-// import Hotel from "./pages/hotel/Hotel";
+import Hotel from "./pages/hotel/Hotel";
 
 
 
