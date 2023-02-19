94d4ef12421b7876254ea9270bf6382ebbfad8bd
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-19 11:15:53 +0200

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="navContent">
+            <span className="logo">Ytech</span>
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+        </div> 
+    </div>
+    
+  )
+}
+
+export default Navbar
