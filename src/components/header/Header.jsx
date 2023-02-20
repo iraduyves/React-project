538cd73c2daf8ae350cf0db1e130ee0c32e4782c
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-20 21:37:37 +0200

    initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,6 +20,7 @@ const Header = () => {
                   <span>Car rentals</span>
             </div>
             <div className="headerListItem">
+              
                  <FontAwesomeIcon icon={faBed}/>
                   <span>Attraction</span>
             </div><div className="headerListItem">
@@ -27,6 +28,20 @@ const Header = () => {
                   <span>Airport taxis</span>
             </div>
          </div>
+         <h1 className="headerTitle">A Lifetime of discounts? It's Genius</h1>
+         <p className="headerDesc">
+          Get rewarded foryour travels-unlock instant savings of 10% or more 
+          with a free Ytech account
+        </p>
+        <button className="headerBtn">Sign in/Register</button>
+        {/* <div className="headerSearch">
+          <div className="headerSearchItem">
+          <FontAwesomeIcon icon={faBed} className="headerIcon"/>
+          <input type="text" placeholder="Where are you going?" className="headerSearchInput" />
+          </div>
+        </div> */}
+
+
         </div>
     </div>
   )
