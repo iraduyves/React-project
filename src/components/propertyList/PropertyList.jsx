e9147ad44382f23f13dbf18963076fd20ddff2ec
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-22 15:09:05 +0200

    initial commit

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -5,7 +5,7 @@ const PropertyList = () => {
   return (
     <div className="pList">
         <div className="pListItem">
-             {/* <img src="" alt="" /></div> */}
+             <img src="" alt="" /></div>
     </div>
   )
 }
