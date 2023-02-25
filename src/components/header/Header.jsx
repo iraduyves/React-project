61b1877717c896dfd7bf115713e7b34aeda0688b
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-25 14:17:49 +0200

    initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -51,7 +51,7 @@ const Header = ({type}) => {
           <h1 className="headerTitle">A Lifetime of discounts? It's Genius</h1>
          <p className="headerDesc">
           Get rewarded foryour travels-unlock instant savings of 10% or more 
-          with a free Ytech accountsssssssssssssssssssssssssssssssss
+          with a free Ytech accountsssssssssssssssssssssssssssssss
 
         </p>
         <button className="headerBtn">Sign in/Register</button>
