0e4a76ca4056061dd518aad1744a15672a0216e9
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-27 22:19:14 +0200

    fix:fixing some bugs

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -81,7 +81,7 @@ const List = () => {
                     className="lsOptionInput"
                     placeholder={options.room}
                   />
-                hjghdgfds
+                hjghdgfd
                   </div>
               </div>
             </div>
