e13ac57618bc3f0a698e543d46ac709528da15cc
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-29 23:48:29 +0200

    feat:adding some location

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -81,7 +81,7 @@ const List = () => {
                     className="lsOptionInput"
                     placeholder={options.room}
                   />    
-                  mnjfdlmsdnglkm,
+                  mnjfdlmsdnglkm
                   </div>
               </div>
             </div>
