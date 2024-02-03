65a36da1d81620d023f160d8d9dca8d7619a08f4
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-03 23:41:37 +0200

    feat:adding some location

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -13,6 +13,7 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  
   return (
     <div>
       <Navbar />
